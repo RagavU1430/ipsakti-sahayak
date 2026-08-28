61ef98ec9d694650e372f7e5c99ce86349050808
RagavU1430 <ragavkrr14@gmail.com>
2026-08-28 12:05:01 +0530

    Add ip-sakti-backend Spring Boot project

---
+++ ip-sakti-backend/src/test/java/com/ipsakti/ip_sakti_backend/IpSaktiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipsakti.ip_sakti_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpSaktiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
